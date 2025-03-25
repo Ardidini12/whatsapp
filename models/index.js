4546c5a04870a641e41c7820a5633c6ca550e4ba
Ardidini12 <161293115+Ardidini12@users.noreply.github.com>
2025-03-25 14:16:31 +0100

    added remote db

--- models/index.js
+++ models/index.js
@@ -13,7 +13,13 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // Local database configuration (commented out)
+  // sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // Remote database configuration
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect
+  });
 }
 
 // Load models dynamically
